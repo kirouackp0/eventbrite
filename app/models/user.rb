e5c92e1e39fd91a0cac802f28af76063f332f839
Kristin Kirouac <kristin.kirouac@gmail.com>
2023-08-08 11:06:12 +0200

    fourth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
+    after_create :welcome_send
+
     has_many :events, through: :attendances
     has_many :attendances
 
     validates :email, presence: true
     validates :encrypted_password, presence: true
-
-    after_create :welcome_send
 
     private
 
